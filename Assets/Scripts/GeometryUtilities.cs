b968e90598a9b088e3b1e781a73e21de95b6e4aa
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-03-02 20:33:37 -0500

    continue writing cdt

--- Assets/Scripts/GeometryUtilities.cs
+++ Assets/Scripts/GeometryUtilities.cs
@@ -3,26 +3,31 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
-public struct Circle2D {
+public struct Circle2D
+{
     Point2D Center { get; set; }
     float Radius { get; set; }
 
-    public Circle2D(float cx, float cy, float r) {
+    public Circle2D(float cx, float cy, float r)
+    {
         Center = new Point2D(cx, cy);
         Radius = r;
     }
 }
 
-public struct Point2D {
+public struct Point2D
+{
     public float X { get; set; }
     public float Y { get; set; }
 
-    public (float, float) XY {
+    public (float, float) XY
+    {
         get { return (X, Y); }
         set { (X, Y) = value; }
     }
 
-    public Point2D(float x, float y) {
+    public Point2D(float x, float y)
+    {
         X = x;
         Y = y;
     }
@@ -33,7 +38,8 @@ public struct Point2D {
     /// <param name="A"></param>
     /// <param name="B"></param>
     /// <returns>Ax * By - Ay * Bx</returns>
-    public static float operator ^(Point2D A, Point2D B) {
+    public static float operator ^(Point2D A, Point2D B)
+    {
         return A.X * B.Y - A.Y * B.X;
     }
 
@@ -43,22 +49,27 @@ public struct Point2D {
     /// <param name="A"></param>
     /// <param name="B"></param>
     /// <returns>Ax * Bx + Ay * By</returns>
-    public static float operator *(Point2D A, Point2D B) {
+    public static float operator *(Point2D A, Point2D B)
+    {
         return A.X * B.X + A.Y * B.Y;
     }
 
     // --- Vector • Scalar Arithmetic Operators --- //
 
-    public static Point2D operator *(Point2D A, float scalar) {
+    public static Point2D operator *(Point2D A, float scalar)
+    {
         return new Point2D(A.X * scalar, A.Y * scalar);
     }
-    public static Point2D operator /(Point2D A, float scalar) {
+    public static Point2D operator /(Point2D A, float scalar)
+    {
         return new Point2D(A.X / scalar, A.Y / scalar);
     }
-    public static Point2D operator +(Point2D A, float scalar) {
+    public static Point2D operator +(Point2D A, float scalar)
+    {
         return new Point2D(A.X + scalar, A.Y + scalar);
     }
-    public static Point2D operator -(Point2D A, float scalar) {
+    public static Point2D operator -(Point2D A, float scalar)
+    {
         return new Point2D(A.X - scalar, A.Y - scalar);
     }
 
@@ -67,62 +78,86 @@ public struct Point2D {
 
 public static class GeometryUtilities
 {
-    public static void Test(Action<string> debug, string toPrint)
-    {
-        debug(toPrint);
-    }
-
-    private static Circle2D Circumcircle(
-        Point2D[] points,
-        (int a, int b, int c) triangle
-    )
+    private static Circle2D Circumcircle(Point2D[] points, (int a, int b, int c) triangle)
     {
         return new Circle2D(0f, 0f, 1f);
     }
 
     /// <summary>
-    /// Normalizes the coordiantes of the provided points between 0 and 1.
+    /// Normalizes the coordinates of the provided points between 0 and 1.
     /// </summary>
-    /// <param name="points"></param>
-    /// <returns></returns>
+    /// <param name="points">The points to normalize</param>
+    /// <returns>Point2D[] — The input `points`, normalized between 0 and 1.</returns>
     public static Point2D[] NormalizePoints(Point2D[] points)
     {
-        var res = points.Aggregate(
-            new
-            {
-                xMin = float.MaxValue,
-                yMin = float.MaxValue,
-                xMax = float.MinValue,
-                yMax = float.MinValue
-            },
-            (acc, point) => new
-            {
-                xMin = Math.Min(acc.xMin, point.X),
-                yMin = Math.Min(acc.yMin, point.Y),
-                xMax = Math.Max(acc.xMax, point.X),
-                yMax = Math.Max(acc.yMax, point.Y),
-            }
+        // MinMax reduction on points
+        var (xMin, yMin, xMax, yMax) = points.Aggregate(
+            (
+                float.MaxValue,
+                float.MaxValue,
+                float.MinValue,
+                float.MinValue
+            ),
+            (acc, point) => (
+                Math.Min(acc.Item1, point.X),
+                Math.Min(acc.Item2, point.Y),
+                Math.Max(acc.Item3, point.X),
+                Math.Max(acc.Item4, point.Y)
+            )
         );
 
-        float dMax = Math.Max(res.xMax - res.xMin, res.yMax - res.yMin);
+        // We divide by the widest difference
+        // between x and y
+        float dMax = Math.Max(xMax - xMin,
+                                yMax - yMin);
 
-        return points
+        var normalizedPoints = points
             .Select(p => new Point2D(
-                    (p.X - res.xMin) / dMax,
-                    (p.Y - res.yMin) / dMax)
-            ).ToArray();
+                    (p.X - xMin) / dMax,  // = x_hat
+                    (p.Y - yMin) / dMax)) // = y_hat
+            .ToArray();
 
+        return normalizedPoints;
     }
 
-    public static (int, int, int)[] DelaunayTriangulation(Point2D[] points)
+    public static (int, int, int)[] ConstrainedDelaunayTriangulation(
+        (int, int)[] constrainedEdges, Point2D[] points)
     {
-        // 1. Create a super-triangle from three points, inside of which all points are contained.
+        // Referencepocket: https://www.newcastle.edu.au/__data/assets/pdf_file/0019/22519/23_A-fast-algortithm-for-generating-constrained-Delaunay-triangulations.pdf
+        // Steps of the algorithm
+        //  1. Normalize points
         Point2D[] normalizedPoints = NormalizePoints(points);
-        // 2. For each point P...
-        //      i.      Insert P into the triangulation. 
-        //      ii.     Find the triangle T that encapsulates P.
-        //      iii.    Create 3 new triangles by drawing an edge between P and each vertex of T.
-        //      iv.     Perform Lawson's Swapping Algorithm.
+        int N = normalizedPoints.Length;
+        int n = (int) Math.Ceiling(Math.Sqrt(N));
+        //  2. Sort into bins (going to skip this step for now)
+        // Point2D[,] bins = new Point2D[n, n];
+
+
+        //  3. Establish the supertriangle, inside of which all points are contained.
+
+        //  4. For each point `P`, perform steps (5-7)
+
+        //  5. Insert `P` into the triangulation. 
+        //      5.1.    Find the triangle `T` that encapsulates `P`.
+        //      5.2.    Create 3 new triangles by drawing an edge between `P` and each vertex of `T`.
+
+        //  6. Initialize a stack for the triangles which are adjacent to edges opposite `P`.
+        //     There are at most three such triangles.
+
+        //  7. Restore the Delaunay triangulation using Lawson's Swapping Algorithm.
+        //      7.1.    Pop a triangle opposite `P` from the stack
+        //      7.2.    If `P` is outside (or on) the circumcurcle for this triangle, return to 7.1.
+        //              Otherwise, the triangle containing `P` as a vertex and the unstacked triangle form a
+        //              convex quadrilateral whose diagonal is drawn in the wrong direction.
+        //                      __             __
+        //              Bad:   |\ |    Good:  | /|
+        //                     |_\|           |/_|
+
+
+
+        List<(int, int, int)> triangles = new();
+
+        triangles.Add((1, 2, 3));
 
         // Lawson's Swapping Algorithm...
         //      i. Put each triangle that has an edge opposite of P on a stack.
--- Assets/Scripts/PointSetDebugView.cs
+++ Assets/Scripts/PointSetDebugView.cs
@@ -5,39 +5,46 @@ using UnityEngine;
 
 public class PointSetDebugView : MonoBehaviour
 {
-    [Range(0.005f, 0.1f)]
     [SerializeField]
+    [Range(0.005f, 0.1f)]
     float pointRadius = 0.025f;
 
-    Vector3 PointToVector3(Vector2 point) {
+    Vector3 PointToVector3(Vector2 point)
+    {
         return new Vector3(point.x, 0, point.y);
     }
 
-    Vector3[] PointsToWorldSpace(Vector2[] points) {
+    Vector3[] PointsToWorldSpace(Vector2[] points)
+    {
         return points
             .Select(point => PointToVector3(point) + gameObject.transform.position)
             .ToArray();
     }
 
-    void ShowPoints(Vector3[] worldPoints) {
+    void ShowPoints(Vector3[] worldPoints)
+    {
         Gizmos.color = Color.magenta;
 
-        foreach (var point in worldPoints) {
+        foreach (var point in worldPoints)
+        {
             Gizmos.DrawSphere(point, pointRadius);
         }
     }
 
-    void ShowTriangles(Vector3[] worldPoints, (int, int, int)[] triangles) {
+    void ShowTriangles(Vector3[] worldPoints, (int, int, int)[] triangles)
+    {
         Gizmos.color = Color.cyan;
 
-        foreach (var (a, b, c) in triangles) {
+        foreach (var (a, b, c) in triangles)
+        {
             Gizmos.DrawLine(worldPoints[a], worldPoints[b]);
             Gizmos.DrawLine(worldPoints[b], worldPoints[c]);
             Gizmos.DrawLine(worldPoints[c], worldPoints[a]);
         }
     }
 
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos()
+    {
         var pts = PointsToWorldSpace(GetComponent<PointSet>().points);
         var tris = GetComponent<PointSet>().triangles;
         ShowPoints(pts);
